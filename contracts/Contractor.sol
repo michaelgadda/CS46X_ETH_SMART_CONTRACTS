215e3e2a24a2f071e3804b684b4d35b1604b06f8
flogindogin <62987541+flogindogin@users.noreply.github.com>
2023-02-01 13:27:13 -0800

    template for contractor and investor

---
+++ contracts/Contractor.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.9;
+
+contract ContractorAcc{
+    address wallet = 0xe53CD8B60182aA08E5Bff56F84491eF48Bc312E0;
+    uint accBal = 1000000;
+    string companyName = "BigBidness";
+}
---
+++ contracts/Investor.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.9;
+
+contract CustomerAcc{
+    address wallet = 0x63A72F52C605448FdE17078c7B9444267D5c11E5;
+    uint accBal = 1000;
+    string customerName = "John";
+    string customerLN = "Doe";
+} 
