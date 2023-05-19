a08fc289f25dd4b2f24b713a2780384b17eb10f9
Abhimanyu Bais <62221878+piedude1717@users.noreply.github.com>
2023-05-18 18:55:03 -0700

    got remaining loan balance up

--- app.js
+++ app.js
@@ -781,6 +781,30 @@ async function balanceOf() {
     }
 }
 
+async function remainingLoanBalance(loanId) {
+    const accounts = await web3.eth.getAccounts();
+
+    // Reference the "remainingLoanBalance" function from smart contract
+    const remainingLoanBalance = contractInstance.methods.remainingLoanBalance(loanId);
+
+    // Call the "remainingLoanBalance" function
+    try {
+        const gasEstimate = await remainingLoanBalance.estimateGas({ from: accounts[0], value: web3.utils.toWei('100', 'ether') });
+        const receipt = await remainingLoanBalance.send({ from: accounts[0], gas: gasEstimate, value: web3.utils.toWei('100', 'ether') });
+        console.log('Transaction receipt:', receipt);
+
+        // Check if the event is present in the transaction receipt
+        if (receipt.events.Lended) {
+            console.log('Remaining loan balance located:', receipt.events.receipt.returnValues);
+        } else {
+            console.log('Error: Event not found in the transaction receipt.');
+        }
+    } catch (error) {
+        console.error(error);
+        console.log('Error: Failed to find the remaining loan balance.');
+    }
+}
+
 
 
 init();
